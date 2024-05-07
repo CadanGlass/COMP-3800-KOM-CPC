309d896a9a90f3119138da17d8ae334d4153964f
kirayoon <113317993+kirayoon@users.noreply.github.com>
2024-05-07 15:59:25 -0700

    update navbar design and implement mobile design

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,35 +1,133 @@
-import React from 'react';
-import { Box, Flex, Link, Spacer } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom';
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 
-const Navbar = () => (
-  <Flex bg='blue.500' color='white' px={4} py={2} align='center'>
-    <Box p='2'>Logo</Box>
-    <Spacer />
-    <Box>
-      <Link as={RouterLink} to='/' px='3'>
-        Home
-      </Link>
-      <Link as={RouterLink} to='/volunteer' px='3'>
-        Volunteer
-      </Link>
-      <Link as={RouterLink} to='/programs' px='3'>
-        Programs
-      </Link>
-      <Link as={RouterLink} to='/events' px='3'>
-        Events
-      </Link>
-      <Link as={RouterLink} to='/newsletter' px='3'>
-        Newsletter
-      </Link>
-      <Link as={RouterLink} to='/about-us' px='3'>
-        About Us
-      </Link>
-      <Link as={RouterLink} to='/contact-us' px='3'>
-        Contact Us
-      </Link>
-    </Box>
-  </Flex>
+import KOM_logo from '../assets/KOM_Logo.png';
+
+const NAV_ITEMS = [
+  {
+    label: 'Volunteer',
+    path: 'volunteer',
+  },
+  {
+    label: 'Programs',
+    path: 'programs',
+  },
+  {
+    label: 'Events',
+    path: 'events',
+  },
+  {
+    label: 'Newsletter',
+    path: 'newsletter',
+  },
+  {
+    label: 'About Us',
+    path: 'about-us',
+  },
+  {
+    label: 'Contact Us',
+    path: 'contact-us',
+  },
+];
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={children.path}
+  >
+    {children.label}
+  </Link>
 );
 
-export default Navbar;
+export default function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ lg: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+
+          <HStack alignItems={'center'}>
+            <Link
+              display='flex'
+              alignItems='center'
+              p={2}
+              rounded={'md'}
+              _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('gray.200', 'gray.700'),
+              }}
+              href='/'
+            >
+              <Avatar size={'md'} src={KOM_logo} />
+              <Text fontSize='lg' fontWeight='bold' ml={2}>
+                KOM CPC
+              </Text>
+            </Link>
+          </HStack>
+
+          <HStack spacing={6} alignItems={'center'}>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', lg: 'flex' }}
+            >
+              {NAV_ITEMS.map((navItem) => (
+                <NavLink key={navItem.path}>{navItem}</NavLink>
+              ))}
+            </HStack>
+            <Button
+              as={Button}
+              fontSize={'sm'}
+              fontWeight={600}
+              color={'white'}
+              bg={'red.600'}
+              href={'#'}
+              // TODO: ADD LINK TO VOLUNTEER PAGE
+              _hover={{
+                bg: 'red.400',
+              }}
+            >
+              Volunteer Now
+            </Button>
+          </HStack>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ lg: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {NAV_ITEMS.map((navItem) => (
+                <NavLink key={navItem.path}>{navItem}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
