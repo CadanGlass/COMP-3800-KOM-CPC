5db3eda0a2683ca5d7aa2f69950d4bd0e4a0a1ef
kirayoon <113317993+kirayoon@users.noreply.github.com>
2024-05-07 14:54:39 -0700

    route navbar items to pages

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Box, Flex, Link, Spacer } from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
 
 const Navbar = () => (
-    <Flex bg="blue.500" color="white" px={4} py={2} align="center">
-        <Box p="2">Logo</Box>
-        <Spacer />
-        <Box>
-            <Link px="3">Volunteer</Link>
-            <Link px="3">Programs</Link>
-            <Link px="3">Events</Link>
-            <Link px="3">Newsletter</Link>
-            <Link px="3">About Us</Link>
-            <Link px="3">Contact Us</Link>
-        </Box>
-    </Flex>
+  <Flex bg='blue.500' color='white' px={4} py={2} align='center'>
+    <Box p='2'>Logo</Box>
+    <Spacer />
+    <Box>
+      <Link as={RouterLink} to='/' px='3'>
+        Home
+      </Link>
+      <Link as={RouterLink} to='/volunteer' px='3'>
+        Volunteer
+      </Link>
+      <Link as={RouterLink} to='/programs' px='3'>
+        Programs
+      </Link>
+      <Link as={RouterLink} to='/events' px='3'>
+        Events
+      </Link>
+      <Link as={RouterLink} to='/newsletter' px='3'>
+        Newsletter
+      </Link>
+      <Link as={RouterLink} to='/about-us' px='3'>
+        About Us
+      </Link>
+      <Link as={RouterLink} to='/contact-us' px='3'>
+        Contact Us
+      </Link>
+    </Box>
+  </Flex>
 );
 
 export default Navbar;
