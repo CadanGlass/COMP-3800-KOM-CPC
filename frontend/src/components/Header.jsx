01a13e8c8aa9db160bab7ed881d33ae822da151e
kirayoon <113317993+kirayoon@users.noreply.github.com>
2024-05-08 16:27:44 -0700

    update contact us page

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import {
+  Container,
+  Image,
+  Heading,
+  Text,
+  Stack,
+  AspectRatio,
+  Button,
+  VStack,
+} from '@chakra-ui/react';
+
+export default function Header({
+  title,
+  description = '',
+  btn1 = '',
+  btn2 = '',
+  imageUrl,
+}) {
+  return (
+    <Container maxW={'8xl'}>
+      <Stack
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 12, md: 16 }}
+        direction={{ base: 'column', md: 'row' }}
+      >
+        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+          <Heading fontWeight={700} fontSize={{ base: '3xl', sm: '4xl' }}>
+            {title}
+          </Heading>
+          {description && <Text color={'gray.600'}>{description}</Text>}
+
+          {btn1 && (
+            <VStack spacing='4' align={{ sm: 'start' }}>
+              <Button>{btn1}</Button>
+              {btn2 && <Button>{btn2}</Button>}
+            </VStack>
+          )}
+        </Stack>
+
+        <AspectRatio flex={1} ratio={6 / 3} minW={{ base: '100%', md: 'auto' }}>
+          <Image
+            rounded={'md'}
+            alt={'feature image'}
+            src={imageUrl}
+            fit={'cover'}
+          />
+        </AspectRatio>
+      </Stack>
+    </Container>
+  );
+}
--- frontend/src/pages/ContactUsPage.jsx
+++ frontend/src/pages/ContactUsPage.jsx
@@ -1,7 +1,147 @@
-import React from 'react'
+import {
+  Container,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  IconButton,
+  Button,
+  VStack,
+  HStack,
+  Wrap,
+  WrapItem,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+  Stack,
+  Icon,
+} from '@chakra-ui/react';
+import {
+  MdPhone,
+  MdEmail,
+  MdLocationOn,
+  MdAccessTimeFilled,
+  MdFacebook,
+  MdOutlineEmail,
+} from 'react-icons/md';
+import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs';
+
+import Header from '../components/Header';
 
 export default function ContactUsPage() {
   return (
-    <div>ContactUsPage</div>
-  )
+    <>
+      <Header
+        title='Contact KOM CPC'
+        description='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore'
+        imageUrl='https://bit.ly/naruto-sage'
+        btn1='Volunteer Now'
+        btn2='Donate Now'
+      />
+
+      <Box py={{ base: 12, md: 16 }}>
+        <Stack spacing={4} as={Container} maxW={'4xl'} textAlign={'center'}>
+          <Heading fontSize={'2xl'}>In An Emergency, Call 9-1-1</Heading>
+          <Text color={'gray.600'} fontSize={'lg'}>
+            Do not contact the KOM CPC in emergencies. If an immediate police
+            response IS NOT required or you need to report crimes after they’ve
+            occurred, contact VPD Non-Emergency at 604.717.3321 or use the
+            Citizen’s Crime Reporting website.
+          </Text>
+        </Stack>
+      </Box>
+
+      <Container maxW={'8xl'} p={4}>
+        <Stack
+          align={'center'}
+          spacing={{ base: 4, md: 0 }}
+          py={{ base: 8, md: 12 }}
+          direction={{ base: 'column', md: 'row' }}
+          bg='gray.50'
+          borderRadius='lg'
+        >
+          <VStack flex={0.5} spacing={{ base: 5, md: 10 }} m={8}>
+            <HStack spacing={4}>
+              <Icon as={MdPhone} color={'gray.500'} />
+              <Text>604 717 3434</Text>
+            </HStack>
+            <HStack spacing={4}>
+              <Icon as={MdEmail} color={'gray.500'} />
+              <Text>test@gmail.com</Text>
+            </HStack>
+            <HStack spacing={4}>
+              <Icon as={MdLocationOn} color={'gray.500'} />
+              <Text>6070 East Boulevard</Text>
+            </HStack>
+            <HStack spacing={4}>
+              <Icon as={MdAccessTimeFilled} color={'gray.500'} />
+              <Text>Monday-Saturday 10am-4pm</Text>
+            </HStack>
+          </VStack>
+
+          <Stack flex={1} minW={{ base: '100%', md: 'auto' }} p={4}>
+            <Box bg='white' borderRadius='lg'>
+              <Box m={8} color='#0B0E3F'>
+                <VStack spacing={5}>
+                  <FormControl id='name'>
+                    <FormLabel>Name</FormLabel>
+                    <InputGroup borderColor='#E0E1E7'>
+                      <InputLeftElement
+                        pointerEvents='none'
+                        children={<BsPerson color='gray.800' />}
+                      />
+                      <Input type='text' size='md' />
+                    </InputGroup>
+                  </FormControl>
+                  <FormControl id='email'>
+                    <FormLabel>Email</FormLabel>
+                    <InputGroup borderColor='#E0E1E7'>
+                      <InputLeftElement
+                        pointerEvents='none'
+                        children={<MdOutlineEmail color='gray.800' />}
+                      />
+                      <Input type='text' size='md' />
+                    </InputGroup>
+                  </FormControl>
+                  <FormControl id='phone'>
+                    <FormLabel>Phone</FormLabel>
+                    <InputGroup borderColor='#E0E1E7'>
+                      <InputLeftElement
+                        pointerEvents='none'
+                        children={<MdPhone color='gray.800' />}
+                      />
+                      <Input type='text' size='md' />
+                    </InputGroup>
+                  </FormControl>
+                  <FormControl id='message'>
+                    <FormLabel>How can we help you?</FormLabel>
+                    <Textarea
+                      borderColor='gray.300'
+                      _hover={{
+                        borderRadius: 'gray.300',
+                      }}
+                      placeholder='Your message here...'
+                    />
+                  </FormControl>
+                  <FormControl id='name' float='right'>
+                    <Button
+                      variant='solid'
+                      bg='#0D74FF'
+                      color='white'
+                      _hover={{}}
+                    >
+                      Send Message
+                    </Button>
+                  </FormControl>
+                </VStack>
+              </Box>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
 }
