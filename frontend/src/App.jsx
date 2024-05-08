edec1605f14279acf19c3fbdc743f2c3ae3f224d
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-05-07 23:09:11 -0700

    Ensure that footer sticks to bottom of page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { ChakraProvider, Box, Text } from '@chakra-ui/react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import NavBar from './components/NavBar';
-import Banner from './components/Banner';
-import Cards from './components/Cards';
-import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import VolunteerPage from './pages/VolunteerPage';
-import ProgramsPage from './pages/ProgramsPage';
-import EventsPage from './pages/EventsPage';
-import NewsletterPage from './pages/NewsletterPage';
-import AboutUsPage from './pages/AboutUsPage';
-import ContactUsPage from './pages/ContactUsPage';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import VolunteerPage from "./pages/VolunteerPage";
+import ProgramsPage from "./pages/ProgramsPage";
+import EventsPage from "./pages/EventsPage";
+import NewsletterPage from "./pages/NewsletterPage";
+import AboutUsPage from "./pages/AboutUsPage";
+import ContactUsPage from "./pages/ContactUsPage";
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/volunteer" element={<VolunteerPage />} />
-          <Route path="/programs" element={<ProgramsPage />} />
-          <Route path="/events" element={<EventsPage />} />
-          <Route path="/newsletter" element={<NewsletterPage />} />
-          <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/contact-us" element={<ContactUsPage />} />
-        </Routes>
-        <Footer />
+        <Box display="flex" flexDirection="column" minHeight="100vh">
+          <NavBar />
+          <Box flex="1">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/volunteer" element={<VolunteerPage />} />
+              <Route path="/programs" element={<ProgramsPage />} />
+              <Route path="/events" element={<EventsPage />} />
+              <Route path="/newsletter" element={<NewsletterPage />} />
+              <Route path="/about-us" element={<AboutUsPage />} />
+              <Route path="/contact-us" element={<ContactUsPage />} />
+            </Routes>
+          </Box>
+          <Footer />
+        </Box>
       </Router>
     </ChakraProvider>
   );
