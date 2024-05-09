6c2bfb610fa4d5092d87ffc6036d35263f32fb10
annettelam <113328885+annettelam@users.noreply.github.com>
2024-05-08 19:58:46 -0700

    Fix card image to be mobile responsive

--- frontend/src/components/programs/ProgramCard.jsx
+++ frontend/src/components/programs/ProgramCard.jsx
@@ -11,14 +11,21 @@ function ProgramCard({ title, description, imageUrl, learnMoreLink, accordionIte
 
     return (
         <Box borderWidth="1px" borderRadius="lg" overflow="hidden" display="flex" flexDirection="column" width="100%">
-            <HStack align="start" spacing="4" p="4" flex="1">
+            <HStack align="start" spacing="4" p="4" flex="1" flexDirection={{ base: "column", md: "row" }}>
                 <VStack align="start" spacing="4" flex="1">
                     <Text fontWeight="bold" fontSize="xl">{title}</Text>
                     <Text>{description}</Text>
                     <Button colorScheme="red" onClick={handleLearnMore}>Learn More →</Button>
                 </VStack>
-                <Box flexShrink="0" width="350px" height="250px" overflow="hidden">
-                    <Image src={imageUrl} alt={`Image of ${title}`} width="100%" height="100%" objectFit="cover" borderRadius="md" />
+                <Box flexShrink="0" width={{ base: "100%", md: "350px" }} height={{ base: "auto", md: "250px" }} overflow="hidden">
+                    <Image
+                        src={imageUrl}
+                        alt={`Image of ${title}`}
+                        width="100%"
+                        height="100%"
+                        objectFit="cover"
+                        borderRadius="md"
+                    />
                 </Box>
             </HStack>
             {showAccordion && (
