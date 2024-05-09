25cf2c31057f0d379fa03038489128a116be1c86
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-05-09 10:49:40 -0700

    make navbar colour consistent with footer

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -63,60 +63,64 @@ export default function Navbar() {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 
 	return (
-		<>
-			<Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-				<Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-					<IconButton
-						size={'md'}
-						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-						aria-label={'Open Menu'}
-						display={{ lg: 'none' }}
-						onClick={isOpen ? onClose : onOpen}
-					/>
+    <>
+      <Box
+        bg={useColorModeValue("gray.800", "gray.900")}
+        color={useColorModeValue("gray.100", "gray.200")}
+        px={4}
+      >
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ lg: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
 
-					<HStack alignItems={'center'}>
-						<Link
-							display="flex"
-							alignItems="center"
-							p={2}
-							rounded={'md'}
-							_hover={{
-								textDecoration: 'none',
-								bg: useColorModeValue('gray.200', 'gray.700'),
-							}}
-							href="/"
-						>
-							<Avatar size={'md'} src={KOM_logo} />
-							<Text fontSize="lg" fontWeight="bold" ml={2}>
-								KOM CPC
-							</Text>
-						</Link>
-					</HStack>
+          <HStack alignItems={"center"}>
+            <Link
+              display="flex"
+              alignItems="center"
+              p={2}
+              rounded={"md"}
+              _hover={{
+                textDecoration: "none",
+                bg: useColorModeValue("gray.200", "gray.700"),
+              }}
+              href="/"
+            >
+              <Avatar size={"md"} src={KOM_logo} />
+              <Text fontSize="lg" fontWeight="bold" ml={2}>
+                KOM CPC
+              </Text>
+            </Link>
+          </HStack>
 
-					<HStack spacing={6} alignItems={'center'}>
-						<HStack
-							as={'nav'}
-							spacing={4}
-							display={{ base: 'none', lg: 'flex' }}
-						>
-							{NAV_ITEMS.map((navItem) => (
-								<NavLink key={navItem.path}>{navItem}</NavLink>
-							))}
-						</HStack>
-						{VolunteerNowButton()}
-					</HStack>
-				</Flex>
+          <HStack spacing={6} alignItems={"center"}>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", lg: "flex" }}
+            >
+              {NAV_ITEMS.map((navItem) => (
+                <NavLink key={navItem.path}>{navItem}</NavLink>
+              ))}
+            </HStack>
+            {VolunteerNowButton()}
+          </HStack>
+        </Flex>
 
-				{isOpen ? (
-					<Box pb={4} display={{ lg: 'none' }}>
-						<Stack as={'nav'} spacing={4}>
-							{NAV_ITEMS.map((navItem) => (
-								<NavLink key={navItem.path}>{navItem}</NavLink>
-							))}
-						</Stack>
-					</Box>
-				) : null}
-			</Box>
-		</>
-	);
+        {isOpen ? (
+          <Box pb={4} display={{ lg: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {NAV_ITEMS.map((navItem) => (
+                <NavLink key={navItem.path}>{navItem}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
 }
