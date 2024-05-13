33fb0fa7f551a88be10df2f616b94782df9c70b7
anothersam22 <stam84@my.bcit.ca>
2024-05-12 20:47:00 -0700

    Create catchment.jsx
    add catchment component

---
+++ frontend/src/components/aboutus/catchment.jsx
@@ -0,0 +1,49 @@
+import {
+	Stack,
+	Heading,
+	Box,
+	useBreakpointValue,
+	Text,
+	Image,
+} from '@chakra-ui/react';
+
+
+import { DefaultCard } from '../DefaultComponents';
+
+
+
+const CatchmentCard = ({ catchmentData }) => {
+	
+	const stackDirection = useBreakpointValue({ base: 'column', xl: 'row' });
+	const stackSpacing = useBreakpointValue({ base: 8, lg: 12, xl: 32 });
+
+	return (
+		<DefaultCard>
+			<Stack direction={stackDirection} spacing={stackSpacing}>
+				<Box flex={1} alignSelf={'center'}>
+                    <Heading as="h2" size="lg" paddingBottom={4}>
+                        {catchmentData.title}
+                    </Heading>
+                    <p> {catchmentData.description} </p>
+                    {catchmentData.areas.map((line, index) => (
+                        <Text key={`line${index}`} paddingBottom={4}>
+                            {line}
+                        </Text>
+                    ))}
+
+				</Box>
+				<Box flex={1} alignSelf={'center'}>
+				
+                    <Image
+						src="https://via.placeholder.com/1000x500"
+						alt="test image"
+						fit="cover"
+						maxW="100%"
+						height="auto"
+					/> 
+				</Box>
+			</Stack>
+		</DefaultCard>
+	);
+};
+export default CatchmentCard;
