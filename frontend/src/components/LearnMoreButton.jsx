3f1c73f6c8f26e656b6e3e39c41db5ab1adcd2f2
annettelam <113328885+annettelam@users.noreply.github.com>
2024-05-13 16:12:34 -0700

    Amend ProgramsPage with new components

---
+++ frontend/src/components/LearnMoreButton.jsx
@@ -0,0 +1,11 @@
+import { Button } from '@chakra-ui/react';
+
+const LearnMoreButton = () => {
+    return (
+        <Button colorScheme="teal" variant="solid">
+            Learn More
+        </Button>
+    );
+};
+
+export default LearnMoreButton;
--- frontend/src/pages/ProgramsPage.jsx
+++ frontend/src/pages/ProgramsPage.jsx
@@ -1,37 +1,26 @@
 import { VStack, useBreakpointValue, Container } from '@chakra-ui/react';
-import VolunteeringActivitiesCard from '../components/volunteer/VolunteeringActivitiesCard';
-import Hero from '../components/volunteer/Hero';
-import AboutCard from '../components/volunteer/About';
-import { DefaultPage } from '../components/DefaultComponents';
-import data from '../test_data/volunteer/volunteer_page.json';
+import ProgramActivitiesCard from '../components/programs/ProgramActivitiesCard';
+import ProgramsHero from '../components/programs/ProgramsHero';
 
-const heroTitle = data.HeroTitle;
-const about = data.About.description;
-const activities = data.Activities[0];
-const why = data.WhyVolunteer[0];
-const faq = data.FAQs[0];
+import { DefaultPage } from '../components/DefaultComponents';
+import data from '../test_data/programs.json';
+
+const heroTitle = data.title;
+const aboutDescription = data.description;
+const programs = data.programs;
 
 export default function ProgramsPage() {
   return (
     <DefaultPage>
-      <Hero title={heroTitle} />
-      <AboutCard aboutData={about} />
-      <VolunteeringActivitiesCard
-        volunteerData={activities.volunteerData}
-        title={activities.title}
-        description={activities.description}
-      />
-      <VolunteeringActivitiesCard
-        volunteerData={activities.volunteerData}
-        title={activities.title}
-        description={activities.description}
-      />
-      <VolunteeringActivitiesCard
-        volunteerData={activities.volunteerData}
-        title={activities.title}
-        description={activities.description}
-      />
-
+      <ProgramsHero title={heroTitle} />
+      {programs.map((program, index) => (
+        <ProgramActivitiesCard
+          key={index}
+          subprograms={program.subprograms}
+          title={program.title}
+          description={program.description}
+        />
+      ))}
     </DefaultPage>
   );
 }
