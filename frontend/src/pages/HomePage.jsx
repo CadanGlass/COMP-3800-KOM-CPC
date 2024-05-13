e31ae1ba3f4b14d110666a076e735599680ed36f
Joseph Chun <joseph.chun@gmail.com>
2024-05-13 15:51:36 -0700

    add map to landing page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   Text,
   VStack,
@@ -8,20 +8,20 @@ import {
   Divider,
   Box,
   Stack,
-} from '@chakra-ui/react';
-import { DefaultPage } from '../components/DefaultComponents';
+} from "@chakra-ui/react";
+import { DefaultPage } from "../components/DefaultComponents";
 
-import Banner from '../components/home/Banner';
-import Cards from '../components/Cards';
-import PartnershipBanner from '../components/home/PartnershipBanner';
-import CallPoliceBanner from '../components/home/CallPoliceBanner';
-import AboutUs from '../components/home/AboutUs';
-import data from '../test_data/home_page.json';
-import SubtextCardWithIcon from '../components/SubtextCardWithIcon';
-import WhoWeAreCard from '../components/home/WhoWeAreCard';
-import Programs from '../components/home/Programs';
+import Banner from "../components/home/Banner";
+import Cards from "../components/Cards";
+import PartnershipBanner from "../components/home/PartnershipBanner";
+import CallPoliceBanner from "../components/home/CallPoliceBanner";
+import AboutUs from "../components/home/AboutUs";
+import data from "../test_data/home_page.json";
+import SubtextCardWithIcon from "../components/SubtextCardWithIcon";
+import WhoWeAreCard from "../components/home/WhoWeAreCard";
+import Programs from "../components/home/Programs";
 
-const url = 'http://localhost:1337/api/programs';
+const url = "http://localhost:1337/api/programs";
 
 export default function HomePage() {
   const [programTitles, setProgramTitles] = useState([]);
@@ -38,7 +38,7 @@ export default function HomePage() {
         setProgramTitles(titles);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setProgramTitles([]);
       });
   }, []);
