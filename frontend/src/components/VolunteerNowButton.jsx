a6c64f5a5496aee321bd24876ec1f70722a92002
annettelam <113328885+annettelam@users.noreply.github.com>
2024-05-13 22:18:46 -0700

    Implement direct link to volunteer app via betterimpact

--- frontend/src/components/VolunteerNowButton.jsx
+++ frontend/src/components/VolunteerNowButton.jsx
@@ -1,20 +1,20 @@
 import { Button } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export default function VolunteerNowButton() {
 	return (
-		<Button
-			as={Button}
-			fontSize={'sm'}
-			fontWeight={600}
-			color={'white'}
-			bg={'red.600'}
-			href={'#'}
-			// TODO: ADD LINK TO VOLUNTEER PAGE
-			_hover={{
-				bg: 'red.400',
-			}}
-		>
-			Volunteer Now
-		</Button>
+		<Link to="https://app.betterimpact.com/PublicOrganization/9751335d-c792-494f-83e0-f504c28ed30c/1" target="_blank" rel="noopener noreferrer">
+			<Button
+				fontSize={'sm'}
+				fontWeight={600}
+				color={'white'}
+				bg={'red.600'}
+				_hover={{
+					bg: 'red.400',
+				}}
+			>
+				Volunteer Now
+			</Button>
+		</Link>
 	);
 }
