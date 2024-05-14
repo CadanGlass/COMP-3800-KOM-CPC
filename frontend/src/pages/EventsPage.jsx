abf3801b755f2424d7945769de8c530e3210ea05
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-05-13 21:21:34 -0700

    Make events page use default components

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -12,11 +12,15 @@ import SearchBar from "../components/eventsPage/SearchBar";
 import CategoryFilter from "../components/eventsPage/CategoryFilter";
 import EventCard from "../components/eventsPage/EventCard";
 
-
+import {
+  DefaultPage,
+  DefaultVStack,
+  DefaultCard,
+} from "../components/DefaultComponents";
 
 const EventsPage = () => {
   const bg = useColorModeValue("white", "gray.800");
-  const cardBg = useColorModeValue("gray.50", "gray.700");
+  const cardBg = useColorModeValue("white", "gray.700");
   const textColor = useColorModeValue("gray.800", "white");
   const borderColor = useColorModeValue("gray.200", "gray.600");
   const hoverBg = useColorModeValue("gray.100", "gray.600");
@@ -39,31 +43,26 @@ const EventsPage = () => {
   ];
 
   return (
-    <Box bg={bg} minHeight="100vh" p={4}>
-      <Flex direction="column" gap={4}>
-        <Heading as="h1" size="xl" textAlign="center" my={4}>
-          KOM CPC Events
-        </Heading>
-        <SearchBar borderColor={borderColor} />
-        <CategoryFilter
-          borderColor={borderColor}
-          textColor={textColor}
-          hoverBg={hoverBg}
-          stackDirection={stackDirection}
-        />
-        <Divider orientation="horizontal" borderColor={borderColor} my={2} />
-        <VStack spacing={4}>
-          {events.map((event, index) => (
-            <EventCard
-              key={index}
-              event={event}
-              cardBg={cardBg}
-              textColor={textColor}
-            />
-          ))}
-        </VStack>
-      </Flex>
-    </Box>
+    <DefaultPage>
+      <Heading as="h1" size="xl" textAlign="center" my={4}>
+        KOM CPC Events
+      </Heading>
+      <SearchBar borderColor={borderColor} />
+      <CategoryFilter
+        borderColor={borderColor}
+        textColor={textColor}
+        hoverBg={hoverBg}
+        stackDirection={stackDirection}
+      />
+      <Divider orientation="horizontal" borderColor={borderColor} my={2} />
+      <DefaultVStack spacing={4}>
+        {events.map((event, index) => (
+          <DefaultCard key={index}>
+            <EventCard event={event} cardBg={cardBg} textColor={textColor} />
+          </DefaultCard>
+        ))}
+      </DefaultVStack>
+    </DefaultPage>
   );
 };
 
