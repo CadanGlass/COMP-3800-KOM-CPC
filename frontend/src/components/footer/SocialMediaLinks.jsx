9ff78d8ca6ae041276167f35c20aaaccbd3996f8
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-05-13 22:14:11 -0700

    Make social media links stand out more

--- frontend/src/components/footer/SocialMediaLinks.jsx
+++ frontend/src/components/footer/SocialMediaLinks.jsx
@@ -1,36 +1,57 @@
-import { Box, Stack, Icon, Link } from "@chakra-ui/react";
+import { Box, Stack, Icon, Link, useColorModeValue } from "@chakra-ui/react";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const SocialMediaLinks = () => {
+  const iconBg = useColorModeValue("gray.200", "gray.700"); // Background color for icons
+  const iconHoverBg = useColorModeValue("gray.300", "gray.600"); // Hover background color
+
   return (
-    <Box p={4} color="white">
+    <Box p={4}>
       <Stack direction="row" spacing={4} justify="center">
         <Link href="https://www.facebook.com/KOMCPC" isExternal>
-          <Icon
-            as={FaFacebook}
-            w={6}
-            h={6}
-            color="#3b5998" // Facebook blue
-            _hover={{ transform: "scale(1.1)" }}
-          />
+          <Box
+            w={10}
+            h={10}
+            bg={iconBg}
+            borderRadius="full"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            _hover={{ bg: iconHoverBg, transform: "scale(1.1)" }}
+            transition="all 0.2s ease-in-out"
+          >
+            <Icon as={FaFacebook} w={5} h={5} color="#3b5998" />
+          </Box>
         </Link>
         <Link href="https://twitter.com/komcpc" isExternal>
-          <Icon
-            as={FaTwitter}
-            w={6}
-            h={6}
-            color="#1DA1F2" // Twitter blue
-            _hover={{ transform: "scale(1.1)" }}
-          />
+          <Box
+            w={10}
+            h={10}
+            bg={iconBg}
+            borderRadius="full"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            _hover={{ bg: iconHoverBg, transform: "scale(1.1)" }}
+            transition="all 0.2s ease-in-out"
+          >
+            <Icon as={FaTwitter} w={5} h={5} color="#1DA1F2" />
+          </Box>
         </Link>
         <Link href="https://www.instagram.com/komcpc" isExternal>
-          <Icon
-            as={FaInstagram}
-            w={6}
-            h={6}
-            color="#E1306C" // Instagram pink
-            _hover={{ transform: "scale(1.1)" }}
-          />
+          <Box
+            w={10}
+            h={10}
+            bg={iconBg}
+            borderRadius="full"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            _hover={{ bg: iconHoverBg, transform: "scale(1.1)" }}
+            transition="all 0.2s ease-in-out"
+          >
+            <Icon as={FaInstagram} w={5} h={5} color="#E1306C" />
+          </Box>
         </Link>
       </Stack>
     </Box>
