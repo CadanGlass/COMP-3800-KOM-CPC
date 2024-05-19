b73c6bedaea637bd4460b4396f24d8aca03b7794
anothersam22 <stam84@my.bcit.ca>
2024-05-18 20:42:44 -0700

    Update about-us.js
    add comment

--- backend/src/api/about-us/routes/about-us.js
+++ backend/src/api/about-us/routes/about-us.js
@@ -8,6 +8,7 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::about-us.about-us', {
 
+    // add the populate middleware to the find and findOne actions
     config: {
         find: {
             middlewares: ['api::about-us.about-page-populate'],
