1cfa8f418a63d3788ca88d3eeb4f3ff85e535ca4
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-05-20 16:54:29 -0700

    center the contact us in footer in mobile mode

--- frontend/src/components/footer/ContactSection.jsx
+++ frontend/src/components/footer/ContactSection.jsx
@@ -5,22 +5,24 @@ import {
   Text,
   HStack,
   VStack,
-  useColorModeValue,
+  useBreakpointValue,
 } from '@chakra-ui/react';
 import { FaPhone, FaEnvelope } from 'react-icons/fa';
 
 const ContactSection = () => {
+  const alignment = useBreakpointValue({ base: 'center', md: 'left' });
+
   return (
     <Box p={1}>
-      <Heading as="h3" size="sm" mb={2}>
+      <Heading as="h3" size="sm" mb={2} textAlign={alignment}>
         Contact
       </Heading>
-      <VStack align="stretch" spacing={1}>
-        <HStack spacing={1}>
+      <VStack align={alignment} spacing={1}>
+        <HStack spacing={1} justifyContent={alignment}>
           <FaPhone />
           <Text fontSize="sm"> (123) 456-7890</Text>
         </HStack>
-        <HStack spacing={1}>
+        <HStack spacing={1} justifyContent={alignment}>
           <FaEnvelope />
           <Text fontSize="sm">email@email.com</Text>
         </HStack>
