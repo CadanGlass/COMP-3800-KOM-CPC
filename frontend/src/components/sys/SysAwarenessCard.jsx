ae4e4f8038624ce638c47c4b46e09c2e0e3877e5
kirayoon <113317993+kirayoon@users.noreply.github.com>
2024-05-21 16:42:17 -0700

    add sys faq

---
+++ frontend/src/components/sys/SysAwarenessCard.jsx
@@ -0,0 +1,18 @@
+import { DefaultCard } from '../DefaultComponents';
+import { Stack, Heading, Box } from '@chakra-ui/react';
+import CustomAccordion from '../AccordionMenu';
+
+export default function SysAwarenessCard({ data }) {
+  return (
+    <DefaultCard>
+      <Stack spacing={4}>
+        <Heading as="h3" size="lg" pb={8}>
+          {data.title}
+        </Heading>
+        <Box>
+          <CustomAccordion accordionData={data.questions} />
+        </Box>
+      </Stack>
+    </DefaultCard>
+  );
+}
