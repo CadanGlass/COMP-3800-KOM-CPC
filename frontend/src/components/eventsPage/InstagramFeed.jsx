184669ceb78a538cf5046870ed6240e705c5c968
CadanGlass <113327002+CadanGlass@users.noreply.github.com>
2024-05-27 00:46:07 -0700

    Add insta feed

--- frontend/src/components/eventsPage/InstagramFeed.jsx
+++ frontend/src/components/eventsPage/InstagramFeed.jsx
@@ -1,5 +1,26 @@
+import React, { useEffect } from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+import { DefaultCard } from '../DefaultComponents';
+
 const InstagramFeed = () => {
-  return <h1>INSTA FEED PLACEHOLDER</h1>;
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = 'https://w.behold.so/widget.js';
+    document.head.appendChild(script);
+  }, []);
+
+  return (
+    <DefaultCard>
+      <Heading as="h2" size="lg" textAlign="center" mb={4}>
+        Instagram Photos
+      </Heading>
+      <Box
+        id="jOgFGHVZpDQA0n8yqzOo"
+        data-behold-id="jOgFGHVZpDQA0n8yqzOo"
+      ></Box>
+    </DefaultCard>
+  );
 };
 
 export default InstagramFeed;
