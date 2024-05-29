f3f4e5982bcf6c170467802e680786f083365999
Joseph Chun <83483639+Mrrm2@users.noreply.github.com>
2024-05-28 17:49:56 -0700

    change homepage events to display 2

--- frontend/src/components/home/EventCard.jsx
+++ frontend/src/components/home/EventCard.jsx
@@ -8,15 +8,17 @@ import {
   AspectRatio,
   Link,
   Stack,
+  ChakraProvider,
 } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import { DefaultCard } from '../DefaultComponents';
+import { keyframes } from '@emotion/react';
 import LearnMoreButton from '../buttons/LearnMoreButton';
 
 export default function EventCard({ name, date, image }) {
   return (
-    <DefaultCard p={8} minHeight="400px">
-      <Box textAlign="center">
+    <DefaultCard p={8} maxHeight="400px">
+      <Box textAlign="center" h={'400px'}>
         <Box display="flex" justifyContent="center" mb={4}>
           {image && (
             <AspectRatio maxH="200px" w="full" ratio={4 / 3} maxWidth="400px">
@@ -30,7 +32,7 @@ export default function EventCard({ name, date, image }) {
           )}
         </Box>
         <Stack spacing={4}>
-          <Heading as="h3" size="lg" mb={2}>
+          <Heading as="h3" size="lg" mb={2} isTruncated>
             {name}
           </Heading>
           <Text fontSize="lg" mb={2}>
