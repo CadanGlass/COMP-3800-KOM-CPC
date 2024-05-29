80da7ce2554f77d908d17f74f443c07c150d3dd1
anothersam22 <stam84@my.bcit.ca>
2024-05-28 20:54:18 -0700

    add additional resource links/contact info on resources page

--- frontend/src/pages/ResourcesPage.jsx
+++ frontend/src/pages/ResourcesPage.jsx
@@ -1,7 +1,7 @@
 // components/ResourcesPage.jsx
 import React, { useState, useEffect } from 'react';
-import { VStack, useColorModeValue } from '@chakra-ui/react';
-import { Section, PageHeading } from '../components/DefaultComponents';
+import { VStack, useColorModeValue, Link, Text, Heading } from '@chakra-ui/react';
+import { Section, PageHeading, DefaultCard } from '../components/DefaultComponents';
 import CommunityPolicingCentresCard from '../components/resources/CommunityPolicingCentresCard';
 import VolunteerCard from '../components/resources/VolunteerCard';
 import WhoToCallCard from '../components/resources/WhoToCallCard';
@@ -69,6 +69,92 @@ const ResourcesPage = () => {
           {resourcesPage.whoToCallCard && (
             <WhoToCallCard data={resourcesPage.whoToCallCard} />
           )}
+        <DefaultCard>
+            <VStack spacing={4} align={'left'}>
+              <Heading as="h2" size="lg">
+                More Resources
+              </Heading>
+              <Text>
+                Links and contacts for more resources and support:
+              </Text>
+              <br />
+
+              <Text>
+                <Link color="teal.500" href="https://www.vpd.ca/" isExternal>
+                  VPD/Fire/Medical Emergency 911
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:911">911</Link> for emergencies only.
+              </Text>
+
+              <Text>
+                <Link color="teal.500" href="https://www.vpd.ca/" isExternal>
+                  VPD non-emergency
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:604-3211">604-3211</Link> for any non-emergency police-related matter.
+              </Text>
+
+              <Text>
+                <Link color="teal.500" href="https://www.vpd.ca/" isExternal>
+                  VPD property office
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:604-717-2726">604-717-2726</Link> for VPD Lost and found.
+              </Text>
+
+              <Text>
+                <Link color="teal.500" href="https://www.vancouver.ca/" isExternal>
+                  City of Vancouver
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:311">311</Link> or <Link color="teal.500" href="tel:604-873-7000">604-873-7000</Link> for bylaws, parking, city parks/properties, and permits.
+              </Text>
+
+              <Text>
+                <Link color="teal.500" href="https://bc.211.ca" isExternal>
+                  BC 211
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:211">211</Link> or <Link color="teal.500" href="tel:1-888-484-3211">1-888-484-3211</Link> for information and referral to a broad range of community, government, and social services.
+              </Text>
+
+              <Text>
+                <Link
+                  color="teal.500"
+                  href="https://www2.gov.bc.ca/gov/content/housing-tenancy/residential-tenancies/contact-the-residential-tenancy-branch"
+                  isExternal
+                >
+                  BC Tenancy Branch
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:604-660-1020">604-660-1020</Link> for landlord-tenant issues.
+              </Text>
+
+              <Text>
+                <Link
+                  color="teal.500"
+                  href="https://www.bccourts.ca/"
+                  isExternal
+                >
+                  Vancouver Civil Court
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:604-660-8989">604-660-8989</Link>.
+              </Text>
+
+              <Text>
+                <Link
+                  color="teal.500"
+                  href="https://www.bccourts.ca/"
+                  isExternal
+                >
+                  Vancouver Criminal Court
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:604-660-4200">604-660-4200</Link>.
+              </Text>
+
+              <Text>
+                <Link color="teal.500" href="https://www.canada.ca/en.html" isExternal>
+                  Government of Canada
+                </Link>{" "}
+                Call <Link color="teal.500" href="tel:1-800-622-6232">1-800-622-6232</Link> for any federal government-related matter.
+              </Text>
+            </VStack>
+          </DefaultCard>
         </VStack>
       </Section>
     </>
